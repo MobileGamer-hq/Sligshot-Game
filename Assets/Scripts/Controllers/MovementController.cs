6ab630fd9e347134e4d8fef4bf5835a5b1141d08
Yorachukwu <yorajideonwo@gmail.com>
2026-03-07 22:59:23 +0100

    Collision for ball
    The ball can now get the position vector 2 of the first contact it makes with a collider

--- Assets/Scripts/Controllers/MovementController.cs
+++ Assets/Scripts/Controllers/MovementController.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+        {
+            //Gets the first contact with a collider tagged {OBSTACLE}
+            Vector2 collisionNormal = collision.GetContact(0).normal;
+        }
+    }
     void Start()
     {
-        
+                
     }
 
-    // Update is called once per frame
     void Update()
     {
         
