91c84cf319d1104ee9dde69532c70a5cdec91584
Yorachukwu <yorajideonwo@gmail.com>
2026-03-07 23:16:33 +0100

    Implemented reflection parameter

--- Assets/Scripts/Controllers/MovementController.cs
+++ Assets/Scripts/Controllers/MovementController.cs
@@ -2,22 +2,32 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public Vector2 ballPosition;
+    public Vector2 ballVelocity;
+
+    Vector2 collisionNormal;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Obstacle"))
         {
             //Gets the first contact with a collider tagged {OBSTACLE}
-            Vector2 collisionNormal = collision.GetContact(0).normal;
+            collisionNormal = collision.GetContact(0).normal;
         }
     }
-    void Start()
+
+    void ReflectBall()
     {
-                
+    //Gets the resultant direction for the ball and normalises
+        Vector2 newDirection = (ballPosition - collisionNormal).normalized;
+
+    //Sets the new velocity of the ball
+        Vector2 newVelocity = newDirection;
     }
+
 
     void Update()
     {
-        
+        ballPosition = gameObject.transform.position;
     }
 }
